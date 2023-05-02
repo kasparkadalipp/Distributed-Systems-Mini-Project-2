6d3abae51cbb6b7d5999b7a52e801d01f02e990d
kasparkadalipp <kaspar.kadalipp@gmail.com>
2023-05-02 22:09:23 +0300

    Implement command "Local-store-ps"

--- node.py
+++ node.py
@@ -1,8 +1,16 @@
+import sys
+import threading
+
 import protocol_pb2
 import protocol_pb2_grpc
 
 
 class Node(protocol_pb2_grpc.BookStoreServiceServicer):
+    def __init__(self, node_id, port):
+        self.dataStoreList = []
+        self.node_id = node_id
+        print(f"Starting node '{node_id}', listening on '{port}'")
+        # TODO start node
 
     def accept_user_input(self):
         user_input = input().strip()
@@ -24,7 +32,7 @@ class Node(protocol_pb2_grpc.BookStoreServiceServicer):
 
         if user_input.startswith('Local-store-ps'):  # <k processes>
             k_processes = int(user_input[14:])
-            # TODO
+            self.dataStoreList = (DataStore(self.node_id, process_id) for process_id in range(1, k_processes+1))
         elif user_input.startswith('Write-operation'):  # <Book, Price>
             name, price = user_input[15:].split(',')
             price = float(price)
@@ -37,13 +45,22 @@ class Node(protocol_pb2_grpc.BookStoreServiceServicer):
             # TODO
 
 
-def main():
-    node = Node()
+class DataStore():
+    def __init__(self, node_id, process_id):
+        self.name = f"Node{node_id}-ps{id}"
 
+def main():
+    if not len(sys.argv) == 2:
+        print("Provide node id")
+        sys.exit(1)
+    node_id = int(sys.argv[1])
+    node_port = 5000 + node_id
+
+    node = Node(node_id, node_port)
     while True:
         try:
             node.accept_user_input()
-        except KeyboardInterrupt | SystemExit:
+        except KeyboardInterrupt:
             print("Exiting...")
             break
 
