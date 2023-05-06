ad8fcab1d9f5aca0a3e31c07a6e9e1ad4762e18d
kasparkadalipp <kaspar.kadalipp@gmail.com>
2023-05-06 23:32:38 +0300

    Correctly format process uuid

--- data_store.py
+++ data_store.py
@@ -1,7 +1,6 @@
 class DataStore:
     def __init__(self, node_id, process_id):
-        self.process_id = process_id
-        self.uuid = f"{node_id}-{process_id}"
+        self.uuid = f"Node{node_id}-ps{process_id}"
         self.books = {}
         self.predecessor = None
         self.successor = None
