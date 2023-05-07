c473c80ddd11c931c060d8a6cba0e86db49dacdb
Daniel Würsch <wuersch.daniel@gmail.com>
2023-05-07 04:14:37 +0300

    Rough initial implementation of most functionality

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 proto:
-	python -m grpc_tools.protoc -I . --python_out=. --grpc_python_out=. protocol.proto
+	python -m grpc_tools.protoc -I . --python_out=. --grpc_python_out=. bookshop.proto
--- data_store.py
+++
@@ -1,6 +0,0 @@
-class DataStore:
-    def __init__(self, node_id, process_id):
-        self.uuid = f"Node{node_id}-ps{process_id}"
-        self.books = {}
-        self.predecessor = None
-        self.successor = None
