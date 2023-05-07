3f2c8638eae1fe1255a0b76d5a5da3f7885bc301
Jkatigb <26495871+jkatigb@users.noreply.github.com>
2023-05-07 08:14:14 +0300

    refactor testnode test suite to only handle one node active, multiple nodes commented out

--- test_bookshop.py
+++ test_bookshop.py
@@ -119,81 +119,31 @@ class TestDataStore(unittest.TestCase):
         self.assertEqual(books_prices[book1], price1)
         self.assertEqual(books_prices[book2], price2)
 
-
-def wait_for_condition(condition_func, timeout=10, interval=0.1):
-    """
-    Waits for a condition to be met or until a timeout is reached.
-
-    :param condition_func: A function that returns a boolean value.
-    :param timeout: The maximum amount of time to wait in seconds. Defaults to 10 seconds.
-    :param interval: The interval between condition checks in seconds. Defaults to 0.1 seconds.
-    :return: True if the condition is met, False if the timeout is reached.
-    """
-    start_time = time.time()
-    while time.time() - start_time < timeout:
-        if condition_func():
-            return True
-        time.sleep(interval)
-    return False
-
-
 class TestNode(unittest.TestCase):
 
     def setUp(self):
-        # etcd3.Etcd3Client(host='localhost', port=2379).delete('/')
-        # etcd3.Etcd3Client(host='localhost', port=2379).delete_prefix('/datastores')
-        # etcd3.Etcd3Client(host='localhost', port=2379).delete_prefix('/node_counter')
-        time.sleep(5)
+        etcd3.Etcd3Client(host='localhost', port=2379).delete_prefix('/')
+        time.sleep(2)
         etcd_host = "localhost"
         etcd_port = 2379
         self.etcd = Etcd(etcd_host, etcd_port)
         self.node = Node(50051)
-        self.node2 = Node(50052)
 
     def tearDown(self):
         self.node.server.stop(0)
-        self.node2.server.stop(0)
         self.etcd.etcd.delete_prefix('/nodes')
         self.etcd.etcd.delete_prefix('/datastores')
         self.etcd.etcd.delete_prefix('/node_counter')
         self.node.datastores = None
         self.node.chain = None
-        self.node2.datastores = None
-        self.node2.chain = None
         self.node = None
-        self.node2 = None
-        time.sleep(5)
+        time.sleep(2)
 
     def test_broadcast_chain(self):
         self.node.local_store_ps(['local-store-ps', '3'])
         self.node.create_chain(['create-chain'])
         self.assertIsNotNone(self.node.chain)
-        self.assertEqual(len(self.node2.chain), 3)
-
-    def test_two_node_local_store_ps(self):
-        self.node.local_store_ps(['local-store-ps', '3'])
-        self.node2.local_store_ps(['local-store-ps', '3'])
-        self.assertEqual(len(self.node.datastores), 3)
-        self.assertEqual(len(self.node2.datastores), 3)
-        self.node.create_chain(['create-chain'])
-        self.assertEqual(len(self.node.chain), 6)
-
-    def test_force_recreate_chain(self):
-        self.node.local_store_ps(['local-store-ps', '3'])
-        self.node.create_chain(['create-chain'])
-        head = self.node.chain[0]
-
-        condition_met = wait_for_condition(lambda: self.node.chain is not None and len(self.node2.chain) == 3,
-                                           timeout=10)
-        self.assertTrue(condition_met, "Chain not created on node within the timeout period.")
-        self.assertIsNotNone(self.node.chain)
-        self.assertEqual(len(self.node2.chain), 3)
-
-        self.node2.local_store_ps(['local-store-ps', '3'])
-        self.node.create_chain(['create-chain', 'force'])
-        self.assertEqual(len(self.node.chain), 6)
-        self.assertNotEqual(head, self.node.chain[0])
-        self.assertIsNotNone(self.node.chain)
+        self.assertEqual(len(self.node.chain), 3)
 
     def test_list_books(self):
         self.node.local_store_ps(['local-store-ps', '3'])
@@ -202,40 +152,33 @@ class TestNode(unittest.TestCase):
         book = "Test Book"
         price = 10
         write_request = bookshop_pb2.WriteRequest(book=book, price=price)
-        node2head_datastore = self.node2.chain[0]
-        with grpc.insecure_channel(node2head_datastore.address) as channel:
+        head = self.node.chain[0]
+        with grpc.insecure_channel(head.address) as channel:
             stub = bookshop_pb2_grpc.DatastoreServiceStub(channel)
-            stub.Write(write_request, timeout=self.node2.timeout)
+            stub.Write(write_request, timeout=self.node.timeout)
 
         books = self.node.list_books([])
         self.assertIn("Test Book", books)
         self.assertIn("10", books)
 
     def test_data_status(self):
-
         self.node.local_store_ps(['local-store-ps', '3'])
-        self.node2.local_store_ps(['local-store-ps', '5'])
-        self.node.create_chain(['create-chain'])
-        self.assertIsNotNone(self.node2.chain)
-        self.assertEqual(len(self.node2.chain), 8)
-        time.sleep(5)
+
+        self.node.create_chain(['create-chain', 'force'])
 
         data_status = self.node.data_status([])
         self.assertEqual("No books in stores", data_status)
-
-        data_status2 = self.node2.data_status([])
-        self.assertEqual("No books in stores", data_status2)
+        self.assertIsNotNone(self.node.chain)
+        self.assertEqual(len(self.node.chain), 3)
         book = "Test Book"
         price = 10
         write_request = bookshop_pb2.WriteRequest(book=book, price=price)
-        head_datastore = self.node2.chain[0]
+        head_datastore = self.node.chain[0]
         with grpc.insecure_channel(head_datastore.address) as channel:
             stub = bookshop_pb2_grpc.DatastoreServiceStub(channel)
             stub.Write(write_request, timeout=self.node.timeout)
         data_status = self.node.data_status([])
-        data_status2 = self.node2.data_status([])
-        self.assertEqual("1) Test Book = 10", data_status)
-        self.assertEqual("1) Test Book = 10", data_status2)
+        self.assertIn("Test Book", data_status)
 
     def test_remove_head(self):
         self.node.local_store_ps(['local-store-ps', '3'])
@@ -245,7 +188,7 @@ class TestNode(unittest.TestCase):
         self.node.remove_head([])
 
         self.assertEqual(len(self.node.removed_heads), 1)
-        self.assertEqual(len(self.node2.chain), 2)
+        self.assertEqual(len(self.node.chain), 2)
 
     def test_restore_head(self):
         self.node.local_store_ps(['local-store-ps', '3'])
@@ -267,10 +210,7 @@ class TestNode(unittest.TestCase):
 
     def test_read_operation(self):
         self.node.local_store_ps(['local-store-ps', '1'])
-        self.node2.local_store_ps(['local-store-ps', '1'])
-        time.sleep(1)
         self.node.create_chain(['create-chain', 'force'])
-        time.sleep(3)
 
         book = "Test Book"
         price = 10
@@ -281,12 +221,11 @@ class TestNode(unittest.TestCase):
             stub.Write(write_request, timeout=self.node.timeout)
             time.sleep(3)
 
-        read = self.node2.read_operation(['read', 'Test Book'])
+        read = self.node.read_operation(['read', 'Test Book'])
         self.assertIn("Test Book", str(read))
 
     def test_write_operation(self):
         self.node.local_store_ps(['local-store-ps', '3'])
-
         self.node.create_chain(['create-chain', 'force'])
 
         self.node.write_operation(['write', '(Test Book, 15.0)'])
@@ -307,6 +246,174 @@ class TestNode(unittest.TestCase):
         self.node.set_timeout(['set-timeout', '2'])
 
         self.assertEqual(self.node.timeout, 120)
+
+# class TestNode(unittest.TestCase):
+#
+#     def setUp(self):
+#         # etcd3.Etcd3Client(host='localhost', port=2379).delete('/')
+#         # etcd3.Etcd3Client(host='localhost', port=2379).delete_prefix('/datastores')
+#         # etcd3.Etcd3Client(host='localhost', port=2379).delete_prefix('/node_counter')
+#         time.sleep(5)
+#         etcd_host = "localhost"
+#         etcd_port = 2379
+#         self.etcd = Etcd(etcd_host, etcd_port)
+#         self.node = Node(50051)
+#         self.node2 = Node(50052)
+#
+#     def tearDown(self):
+#         self.node.server.stop(0)
+#         self.node2.server.stop(0)
+#         self.etcd.etcd.delete_prefix('/nodes')
+#         self.etcd.etcd.delete_prefix('/datastores')
+#         self.etcd.etcd.delete_prefix('/node_counter')
+#         self.node.datastores = None
+#         self.node.chain = None
+#         self.node2.datastores = None
+#         self.node2.chain = None
+#         self.node = None
+#         self.node2 = None
+#         time.sleep(5)
+#
+#     def test_broadcast_chain(self):
+#         self.node.local_store_ps(['local-store-ps', '3'])
+#         self.node.create_chain(['create-chain'])
+#         self.assertIsNotNone(self.node.chain)
+#         self.assertEqual(len(self.node2.chain), 3)
+#
+#     def test_two_node_local_store_ps(self):
+#         self.node.local_store_ps(['local-store-ps', '3'])
+#         self.node2.local_store_ps(['local-store-ps', '3'])
+#         self.assertEqual(len(self.node.datastores), 3)
+#         self.assertEqual(len(self.node2.datastores), 3)
+#         self.node.create_chain(['create-chain'])
+#         self.assertEqual(len(self.node.chain), 6)
+#
+#     def test_force_recreate_chain(self):
+#         self.node.local_store_ps(['local-store-ps', '3'])
+#         self.node.create_chain(['create-chain'])
+#         head = self.node.chain[0]
+#
+#         self.assertIsNotNone(self.node.chain)
+#         self.assertEqual(len(self.node2.chain), 3)
+#
+#         self.node2.local_store_ps(['local-store-ps', '3'])
+#         self.node.create_chain(['create-chain', 'force'])
+#         self.assertEqual(len(self.node.chain), 6)
+#         self.assertNotEqual(head, self.node.chain[0])
+#         self.assertIsNotNone(self.node.chain)
+#
+#     def test_list_books(self):
+#         self.node.local_store_ps(['local-store-ps', '3'])
+#         self.node.create_chain(['create-chain', 'force'])
+#
+#         book = "Test Book"
+#         price = 10
+#         write_request = bookshop_pb2.WriteRequest(book=book, price=price)
+#         node2head_datastore = self.node2.chain[0]
+#         with grpc.insecure_channel(node2head_datastore.address) as channel:
+#             stub = bookshop_pb2_grpc.DatastoreServiceStub(channel)
+#             stub.Write(write_request, timeout=self.node2.timeout)
+#
+#         books = self.node.list_books([])
+#         self.assertIn("Test Book", books)
+#         self.assertIn("10", books)
+#
+#     def test_data_status(self):
+#         self.node.local_store_ps(['local-store-ps', '3'])
+#         time.sleep(3)
+#         self.node2.local_store_ps(['local-store-ps', '5'])
+#         time.sleep(3)
+#         self.node.create_chain(['create-chain', 'force'])
+#         time.sleep(3)
+#
+#         data_status = self.node.data_status([])
+#         self.assertEqual("No books in stores", data_status)
+#         self.assertIsNotNone(self.node2.chain)
+#         self.assertEqual(len(self.node2.chain), 8)
+#         data_status2 = self.node2.data_status([])
+#         self.assertEqual("No books in stores", data_status2)
+#         book = "Test Book"
+#         price = 10
+#         write_request = bookshop_pb2.WriteRequest(book=book, price=price)
+#         head_datastore = self.node2.chain[0]
+#         with grpc.insecure_channel(head_datastore.address) as channel:
+#             stub = bookshop_pb2_grpc.DatastoreServiceStub(channel)
+#             stub.Write(write_request, timeout=self.node.timeout)
+#         data_status = self.node.data_status([])
+#         data_status2 = self.node2.data_status([])
+#         self.assertEqual("1) Test Book = 10", data_status)
+#         self.assertEqual("1) Test Book = 10", data_status2)
+#
+#     def test_remove_head(self):
+#         self.node.local_store_ps(['local-store-ps', '3'])
+#
+#         self.node.create_chain(['create-chain', 'force'])
+#
+#         self.node.remove_head([])
+#
+#         self.assertEqual(len(self.node.removed_heads), 1)
+#         self.assertEqual(len(self.node2.chain), 2)
+#
+#     def test_restore_head(self):
+#         self.node.local_store_ps(['local-store-ps', '3'])
+#
+#         self.node.create_chain(['create-chain', 'force'])
+#
+#         time.sleep(3)
+#
+#         self.node.remove_head([])
+#
+#         self.assertEqual(len(self.node.removed_heads), 1)
+#
+#         time.sleep(3)
+#
+#         result = self.node.restore_head([])
+#
+#         # self.assertEqual(len(self.node.removed_heads), 0)
+#         self.assertIn("Successfully instituted", result)
+#
+#     def test_read_operation(self):
+#         self.node.local_store_ps(['local-store-ps', '1'])
+#         self.node2.local_store_ps(['local-store-ps', '1'])
+#         time.sleep(1)
+#         self.node.create_chain(['create-chain', 'force'])
+#         time.sleep(3)
+#
+#         book = "Test Book"
+#         price = 10
+#         write_request = bookshop_pb2.WriteRequest(book=book, price=price)
+#         head_datastore = self.node.chain[0]
+#         with grpc.insecure_channel(head_datastore.address) as channel:
+#             stub = bookshop_pb2_grpc.DatastoreServiceStub(channel)
+#             stub.Write(write_request, timeout=self.node.timeout)
+#             time.sleep(3)
+#
+#         read = self.node2.read_operation(['read', 'Test Book'])
+#         self.assertIn("Test Book", str(read))
+#
+#     def test_write_operation(self):
+#         self.node.local_store_ps(['local-store-ps', '3'])
+#
+#         self.node.create_chain(['create-chain', 'force'])
+#
+#         self.node.write_operation(['write', '(Test Book, 15.0)'])
+#
+#         books = self.node.list_books([])
+#
+#         self.assertIn("Test Book", books)
+#
+#         read_request = bookshop_pb2.ReadRequest(books=['Test Book'], allow_dirty=True)
+#         tail_datastore = self.node.chain[-1]
+#         with grpc.insecure_channel(tail_datastore.address) as channel:
+#             stub = bookshop_pb2_grpc.DatastoreServiceStub(channel)
+#             response = stub.Read(read_request, timeout=self.node.timeout)
+#
+#         self.assertEqual("Test Book", response.books[0].book)
+#
+#     def test_set_timeout(self):
+#         self.node.set_timeout(['set-timeout', '2'])
+#
+#         self.assertEqual(self.node.timeout, 120)
 
 
 if __name__ == "__main__":
